d6b0f8bd8f1f1dc78335a441e14c7a1cfb854118
Your Name <you@example.com>
2022-07-19 19:54:32 -0600

    cookie_no knowledge check

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-function cookie (details) {
-Cookies.set(`button_clicked`, `yes`)
+function cookie(details) {
+    Cookies.set(`button_clicked`, `yes`);
 }
-function delete_cookie (details) {
-     Cookies.remove(`button_clicked`); 
+function delete_cookie(details) {
+    Cookies.remove(`button_clicked`);
 }
 
 let cookie_but = document.getElementById(`cookie_button`);
-cookie_but.addEventListener(`click`, cookie)
- let cookiez = Cookies.get(`button_clicked`)
- if(cookiez !== undefined) {
-     document.body.insertAdjacentHTML(`afterEnd`, `<h1>you baked a cookie</h1>`)
+cookie_but.addEventListener(`click`, cookie);
+let cookiez = Cookies.get(`button_clicked`);
+if (cookiez !== undefined) {
+    document.body.insertAdjacentHTML(`afterEnd`, `<h1>you baked a cookie</h1>`)
 
- }
-
-
-
+}
 
 let dont_click = document.getElementById(`second_button`);
-dont_click.addEventListener(`click`, delete_cookie )
+dont_click.addEventListener(`click`, delete_cookie);
 
 
 
 
+function cookie_no(details) {
+Cookies.set(`button_clicked`, `no`);
+}
 
-
-
+let cookie_noo = document.getElementById(`button_three`)
+cookie_noo.addEventListener(`click`, cookie_no)
