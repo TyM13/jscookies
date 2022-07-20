49adaef08cdfbf982632ae392e1a111f3ef3a458
Your Name <you@example.com>
2022-07-19 19:19:53 -0600

    knowledge check backe some cookies need to pratice more

--- js/app.js
+++ js/app.js
@@ -4,3 +4,17 @@ Cookies.set(`button_clicked`, `yes`)
 
 let cookie_but = document.getElementById(`cookie_button`);
 cookie_but.addEventListener(`click`, cookie)
+ let cookiez = Cookies.get(`button_clicked`)
+ if(cookiez !== undefined) {
+     document.body.insertAdjacentHTML(`afterEnd`, `<h1>you baked a cookie</h1>`)
+ }
+
+
+
+
+
+
+
+
+
+
